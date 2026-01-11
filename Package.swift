f06aa244c333e25400f41291d0d30f4b559d0da7
mingdw <mingddww@gmail.com>
2026-01-11 16:11:50 +0800

    Fix iOS support: add AppKit conditional compilation and iOS 17 availability for LaTeX features

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "swift-markdown-ui",
   platforms: [
-    .macOS(.v12),
+    .macOS(.v13),
     .iOS(.v15),
     .tvOS(.v15),
     .macCatalyst(.v15),
